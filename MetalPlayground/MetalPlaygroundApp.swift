2e458b2812de038c75fa4a3a786a692604a67b20
Todd Martin <kj4ntv@gmail.com>
2024-02-27 20:53:29 -0800

    Initial Commit

---
+++ MetalPlayground/MetalPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalPlaygroundApp.swift
+//  MetalPlayground
+//
+//  Created by Todd Martin on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MetalPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
